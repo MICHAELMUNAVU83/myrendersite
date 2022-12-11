3c812fbe357f26567fefb0dee100eabf09e07f58
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-12-11 13:21:01 +0300

    Added create

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -3,4 +3,13 @@ class PlantsController < ApplicationController
         @plants = Plant.all
         render json: @plants
     end
+    def create
+        @plant = Plant.create(plant_params)
+        render json: @plant
+    end
+
+    private
+    def plant_params
+        params.require(:plant).permit(:name)
+    end
 end
